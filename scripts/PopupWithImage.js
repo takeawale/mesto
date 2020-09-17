e858536cc45ffa064c8e527347488e909363ffa9
luiza <takeawale@yandex.ru>
2020-09-17 23:19:22 +0500

    9

--- scripts/PopupWithImage.js
+++ scripts/PopupWithImage.js
@@ -4,15 +4,17 @@ import Popup from './popup.js';
 export default class PopupWithImage extends Popup {
   constructor(popupSelector) {
     super(popupSelector);
+    
+    this._popupImage = this._popup.querySelector('.popup__image');
+    this._popupCaption = this._popup.querySelector('.popup__caption')
   }
 
 
   open(link, name) {
     super.open();
-    this._popup.querySelector('.popup__image').src = link;
-    this._popup.querySelector('.popup__caption').textContent = name;
+    this._popupImage.src = link;
+    this._popupCaption.textContent = name;
     console.log('3we')
   }
 
 }
-
